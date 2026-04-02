866488af308d0370d4e8d6394ac905d075bc475e
Satish Babu Karra <karrasatishbabu@gmail.com>
2026-04-02 12:35:25 -0500

    home page

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <footer className='footer py-3 my-3'>
+        <hr className='border-bottom' />
+        <p className='text-light text-center'>&copy;2026 • Built with ❤️ by Satish</p>
+
+        </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+<>
+    <div className='container'>
+        <div className='p-5 text-center bg-light-dark rounded'>
+            <h1 className='text-light'>Stock Prediction Portal</h1>
+            <p className='text-light' href="">This stock prediction application uses machine learning technologies like Keras, TensorFlow, and LSTM to analyze stock data and predict future prices. It focuses on key indicators such as 100-day and 200-day moving averages to help users make better investment decisions.</p>
+            <Button text='Login' class="btn-outline-info" />
+        </div>
+
+    </div>
+</> 
+  )
+}
+
+export default Main
