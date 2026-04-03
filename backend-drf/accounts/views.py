cab97dd808b0b05066892d748e44e5556f5b2f62
Satish Babu Karra <karrasatishbabu@gmail.com>
2026-04-03 15:43:35 -0500

    registration backend/api

---
+++ backend-drf/accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSeriazer
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+
+# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSeriazer
+    permission_classes = [AllowAny]
+
